f6a8661fd40f74b3da694b18415bde502b8a04bd
Johannes Zeller <johannes_zeller@sluz.ch>
2022-09-09 13:29:24 +0200

    solve task 1

--- ex1/main.go
+++ ex1/main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Johannes"
+	var lastName string = "Zeller"
+	var dayOfBirth int = 1
+	var monthOfBirth int = 4
+	var yearOfBirth int = 2006
+	var numberOfSiblings int = 2
+	var heightInMeters float32 = 1.85
+	var zodiacSign rune = '\u2648'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
